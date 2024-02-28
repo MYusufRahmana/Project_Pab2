fb06dad1247a34dcb2ba15de4d081485274c1b70
UcupBoys <yusufrahmana14@gmail.com>
2024-02-28 08:27:18 +0700

    fix ui main for aplikasikaryawan

--- aplikasikaryawan/lib/main.dart
+++ aplikasikaryawan/lib/main.dart
@@ -32,7 +32,46 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Daftar Karyawan'),
+      ),
+      body: FutureBuilder<List<Karyawan>>(
+        future: _readJsonData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    snapshot.data![index].nama,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Umur: ${snapshot.data![index].umur}'),
+                      Text(
+                          'Alamat: ${snapshot.data![index].alamat.jalan}, ${snapshot.data![index].alamat.kota}, ${snapshot.data![index].alamat.provinsi}'),
+                      Text('Hobi: ${snapshot.data![index].hobi.join(', ')},'),
+                    ],
+                  ),
+                  leading: FlutterLogo(
+                    size: 60,
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error : ${snapshot.error}'));
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
   }
 }
