c5f64b232d313969a759e48cac116f2b4632ef0b
UcupBoys <yusufrahmana14@gmail.com>
2024-02-28 09:31:10 +0700

    firts commit
    make:
    models
    screens
    services

---
+++ pilem_application/README.md
@@ -0,0 +1,3 @@
+# pilem_application
+
+A new Flutter project.
---
+++ pilem_application/android/app/src/main/kotlin/com/example/pilem_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pilem_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
